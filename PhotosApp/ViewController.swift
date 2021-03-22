89af25270af70a1c8125a3b1f9ef60c280c9ca2a
zzisun <jisunkim98@gmail.com>
2021-03-22 16:33:54 +0900

    Add: CollectionView
    Adopt UICollectionViewDataSource
    Set cell color using UIColor

--- PhotosApp/ViewController.swift
+++ PhotosApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.collectionView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 40
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "colorCell", for: indexPath)
+        cell.contentView.backgroundColor = UIColor.random
+        return cell
+    }
+}
---
+++ PhotosAppTests/PhotosAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosAppTests.swift
+//  PhotosAppTests
+//
+//  Created by 김지선 on 2021/03/22.
+//
+
+import XCTest
+@testable import PhotosApp
+class PhotosAppTests: XCTestCase {
+
+    func testColor(){
+
+    }
+
+}
