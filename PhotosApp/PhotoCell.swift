4e67484fa5c0e2955890eebdf70e66f8c5c35e73
ellyheetov <yaaajcm@naver.com>
2021-03-23 11:44:13 +0900

    Add PhotoCell

---
+++ PhotosApp/PhotoCell.swift
@@ -0,0 +1,12 @@
+//
+//  PhotoCell.swift
+//  PhotosApp
+//
+//  Created by 박혜원 on 2021/03/23.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
--- PhotosApp/ViewController.swift
+++ PhotosApp/ViewController.swift
@@ -6,13 +6,23 @@
 //
 
 import UIKit
+import Photos
+import PhotosUI
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var collectionView: UICollectionView!
+//    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var allOfPhotos : PHFetchResult<PHAsset>!
+    let imageManager = PHCachingImageManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.collectionView.dataSource = self
+//        self.collectionView.dataSource = self
+        
+        allOfPhotos = PHFetchResult()
+        allOfPhotos = PHAsset.fetchAssets(with: nil)
+        
     }
 
 
@@ -20,12 +30,18 @@ class ViewController: UIViewController {
 
 extension ViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 40
+        return allOfPhotos.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "colorCell", for: indexPath)
-        cell.contentView.backgroundColor = UIColor.random
+
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath)
+        
+        imageManager.requestImage(for: allOfPhotos.object(at: indexPath.item), targetSize: CGSize(width: 100, height: 100), contentMode: .aspectFit, options: nil, resultHandler: { uiimage, dict  in
+            
+        })
+        
+//        cell.contentView.backgroundColor = UIColor.random
         return cell
     }
 }
